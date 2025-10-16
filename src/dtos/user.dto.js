5263cf1959053371dbd0a47f6985f74e7da4f3ee
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-10-16 21:06:28 +0900

    [Feat] 5주차 실습: 회원가입API 구현

---
+++ src/dtos/user.dto.js
@@ -0,0 +1,32 @@
+export const bodyToUser = (body) => {
+  const birth = new Date(body.birth);
+
+  return {
+    email: body.email,
+    name: body.name,
+    gender: body.gender,
+    birth: birth,
+    address: body.address || "",
+    detailAddress: body.detailAddress || "",
+    phoneNumber: body.phoneNumber,
+    preferences: body.preferences,
+  };
+};
+
+export const responseFromUser = ({ user, preferences }) => {
+  if (user === null) {
+    return null;
+  }
+
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    gender: user.gender,
+    birth: user.birth,
+    address: user.address,
+    detailAddress: user.detailAddress,
+    phoneNumber: user.phone_number,
+    preferences: preferences,
+  };
+};
