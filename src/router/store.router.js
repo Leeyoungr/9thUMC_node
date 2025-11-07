2c00ca4702078c77e8359485e4e86eb35be41f59
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-11-07 09:52:55 +0900

    [Docs] 주석추가

--- src/router/store.router.js
+++ src/router/store.router.js
@@ -2,27 +2,18 @@ import { Router } from "express";
 
 import { handleStoreCreate } from "../controllers/store.controller.js";
 
-import {
-  createStoreReqHandler,
-  validateCreateStoreReq,
-} from "../middleware/validation/store.validation.js";
+import { createStoreReqHandler, validateCreateStoreReq } from "../middleware/validation/store.validation.js";
 
 import { handleCreateStoreReview } from "../controllers/review.controller.js";
 import { ensureStoreExists } from "../middleware/store.middleware.js";
-import {
-  createReviewReqHandler,
-  validateCreateReviewReq,
-} from "../middleware/validation/review.validation.js";
+import { createReviewReqHandler, validateCreateReviewReq } from "../middleware/validation/review.validation.js";
 
 const router = Router();
 
-router.post(
-  "/signup",
-  validateCreateStoreReq,
-  createStoreReqHandler,
-  handleStoreCreate
-);
+// POST /api/v1/stores/signup -> 매장 등록
+router.post("/signup", validateCreateStoreReq, createStoreReqHandler, handleStoreCreate);
 
+// POST /api/v1/stores/:storeId/reviews -> 매장 리뷰 생성
 router.post(
   "/:storeId/reviews",
   validateCreateReviewReq,
--- src/router/user.router.js
+++ src/router/user.router.js
@@ -4,6 +4,7 @@ import { handleUserSignUp } from "../controllers/user.controller.js";
 
 const router = Router();
 
+// POST /api/v1/users/signup -> 사용자 회원가입
 router.post("/signup", handleUserSignUp);
 
 export default router;
