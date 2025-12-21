f4fa5d2c92341b6dad29d7553e3dc6f2045b3559
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-12-21 22:39:47 +0900

    [Fix] 리디렉션 URI 수정

--- src/util/google.util.js
+++ src/util/google.util.js
@@ -47,7 +47,7 @@ export const googleStrategy = new GoogleStrategy(
   {
     clientID: process.env.PASSPORT_GOOGLE_CLIENT_ID,
     clientSecret: process.env.PASSPORT_GOOGLE_CLIENT_SECRET,
-    callbackURL: "/oauth2/callback/google",
+    callbackURL: "/auth/callback/google",
     scope: ["email", "profile"],
   },
 
